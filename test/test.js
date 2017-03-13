bcd97b2a2cd5529f10d3fdd4c345fdfc5696ec84
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-03-13 12:34:27 -0700

    Worked on making new moves - have a bug

--- test/test.js
+++ test/test.js
@@ -2,23 +2,24 @@ var assert = require('assert');
 var Board = require('../tictactoe.js');
 var testBoard = new Board();
 var initBoard = '   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n';
+var afterOneMove = ' X |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n';
 
 describe('ticTacToe', function() {
 
-  describe('game initialization', function() {
+  describe('The game', function() {
+
     it('should tell who\'s turn it is', function() {
       assert.equal(1, testBoard.curPlayer);
     });
-
     it('should have an empty board', function() {
       assert.equal(initBoard, testBoard.board);
     });
 
   });
 
-  describe('Board should exist', function() {
-    it('should return true if board exists', function() {
-      assert.equal(true, testBoard.exists);
+  describe('Players', function() {
+    it('should be able to submit moves', function() {
+      assert.equal(afterOneMove, testBoard.logMove(1, one));
     });
   });
 
--- tictactoe.js
+++ tictactoe.js
@@ -1,29 +1,39 @@
 class Board {
-  constructor(init) {
-    init = ' ';
+  constructor() {
     this.curPlayer = 1;
-    this.exists = true;
-    this.board = ` ${this.one = init} | ${this.two = init} | ${this.three = init} \n-----------\n ${this.four = init} | ${this.five = init} | ${this.six = init} \n-----------\n ${this.seven = init} | ${this.eight = init} | ${this.nine = init} \n`;
+
+    this.one = ' ';
+    this.two = ' ';
+    this.three = ' ';
+    this.four = ' ';
+    this.five = ' ';
+    this.six = ' ';
+    this.seven = ' ';
+    this.eight = ' ';
+    this.nine = ' ';
+
+    this.board = ` ${this.one} | ${this.two} | ${this.three} \n-----------\n ${this.four} | ${this.five} | ${this.six} \n-----------\n ${this.seven} | ${this.eight} | ${this.nine} \n`;
   }
   logBoard(){
     console.log(`it is player ${this.curPlayer}'s turn`);
     console.log(this.board);
-    console.log(`Please make your move player: ${this.curPlayer}`);
-    console.log(`\n`);
     return;
+  }
+  logMove(curPlayer, position) {
+    if (curPlayer === 1) {
+      this[position] = "X";
+    } else {
+      this[position] = "O"
+    }
+    return;
+    // is already taken (edge case)
   }
 }
 
 var myBoard = new Board();
+myBoard.logMove(1, "one");
 myBoard.logBoard();
-
-
-
-
-
-
-
-
+console.log(myBoard.board);
 
 
 
